08b773e8fdebd1015406dda7af9a64ec25e7942d
Mustafa Sarshar <mustafa.sarshar88@gmail.com>
2024-06-09 13:08:46 +0200

    init repo

---
+++ android/app/src/main/java/io/ionic/app/plugins/MainActivity.java
@@ -0,0 +1,5 @@
+package io.ionic.app.plugins;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "io.ionic.app.plugins",
+  appName: "Ionic App Plugins",
+  webDir: "www",
+};
+
+export default config;
